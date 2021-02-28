6d5827dff35fbeb2dcf42a3ce89991d1e9459bce
Roman Dobias <rom.dobias@gmail.com>
2021-02-28 19:23:56 +0100

    Added unittest for SuperTux

--- tests/unittests/pipeline/pipeline_injector_test.cpp
+++ tests/unittests/pipeline/pipeline_injector_test.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "pipeline/pipeline_injector.hpp"
+#include <GL/gl.h>
 using namespace ve;
 using namespace ve::pipeline;
 namespace helper
@@ -112,4 +113,111 @@ TEST(PipelineInjector, Injection) {
     if(result.metadata)
         helper::printMetadata(*result.metadata);
 }
+
+TEST(PipelineInjector, InOutTest) {
+
+    PipelineInjector injector;
+
+    PipelineInjector::PipelineType input =
+    {
+        {GL_VERTEX_SHADER, R"(
+            #version 330
+            in vec3 vertex;
+
+            out vec2 uv;
+            
+            uniform mat4 mvp;
+            void main()
+            {
+                normal = vertex.xy;
+                gl_Position = mvp*vec4(vertex);
+            }
+        )"},
+        {GL_FRAGMENT_SHADER, R"(
+            #version 330
+
+            // Wrapper to allow easy sampling for material texture layers
+#define HIGH_SAMPLING   4.0
+#define MEDIUM_SAMPLING 2.0
+#define LOW_SAMPLING    1.0
+
+            vec4 sampleTextureLayer0(vec2 uv)
+            {
+                return texture(tex_layer_0, uv);
+            }
+
+
+            vec4 multi_sampleTextureLayer3(vec2 uv, float distance)
+            {
+
+                vec4 l_col = sampleTextureLayer3(uv * LOW_SAMPLING);
+                vec4 m_col = sampleTextureLayer3(uv * MEDIUM_SAMPLING);
+                vec4 h_col = sampleTextureLayer3(uv * HIGH_SAMPLING);
+
+                // From Low to medium
+                float factor = distance * 0.02;
+                factor = pow(factor, 2.5);
+                factor = clamp(factor, 0.0, 1.0);
+                vec4 f_col = mix(m_col, l_col, factor);
+                
+                // From medium to high
+                factor = distance * 0.1;
+                factor = pow(factor, 2.5);
+                factor = clamp(factor, 0.0, 1.0);
+
+                f_col = mix(h_col, f_col, factor);
+
+                return f_col;
+            }
+
+            vec4 sampleTextureLayer4(vec2 uv)
+            {
+                return texture(tex_layer_4, uv);
+            }
+
+            vec4 multi_sampleTextureLayer4(vec2 uv, float distance)
+            {
+
+                vec4 l_col = sampleTextureLayer4(uv * LOW_SAMPLING);
+                vec4 m_col = sampleTextureLayer4(uv * MEDIUM_SAMPLING);
+                vec4 h_col = sampleTextureLayer4(uv * HIGH_SAMPLING);
+
+                // From Low to medium
+                float factor = distance * 0.02;
+                factor = pow(factor, 2.5);
+                factor = clamp(factor, 0.0, 1.0);
+                vec4 f_col = mix(m_col, l_col, factor);
+                
+                // From medium to high
+                factor = distance * 0.1;
+                factor = pow(factor, 2.5);
+                factor = clamp(factor, 0.0, 1.0);
+
+                f_col = mix(h_col, f_col, factor);
+
+                return f_col;
+            }
+
+            vec4 sampleTextureLayer5(vec2 uv)
+            {
+                return texture(tex_layer_5, uv);
+            }
+
+
+            in vec2 uv;
+            out vec4 color;
+            void main()
+            {
+                color = vec4(uv, 0.0,0.0);
+            }
+        )"},
+
+    };
+
+    auto result = injector.process(input);
+    ASSERT_TRUE(result.metadata);
+    // All `vec2 uv` should be replaced with corresponding enhancer_frag_ prefix.
+    ASSERT_TRUE((result.pipeline[GL_FRAGMENT_SHADER]).find(" uv") == std::string::npos);
+}
+
 }
